50eb28a3bb1b5790e9d22abe7b079ad732995841
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2024-12-22 19:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function convertToRoman(num) {
+	if (num <= 0 || num > 100000) {
+    return "Invalid input"; // Handle numbers outside the range
+  }
+	
   	const obj = {
       0:['M',1000], 
       1:['D', 500], 
@@ -9,12 +13,23 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
+	 let romanNumeral = '';
+
+  for (let [symbol, value] of romanSymbols) {
+    while (num >= value) {
+      romanNumeral += symbol; // Append the symbol
+      num -= value;           // Subtract the value
+    }
+  }
+
+  return romanNumeral;
+
   //your code here
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
